14a4ea9b6c550553a5a4768b9b7bcfc0bb7868ed
Shubham Ghodkhande <shubhamg0615@gmail.com>
2023-10-10 21:09:05 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+//components
+import Connection from './database/db.js';
+import Router from './routes/route.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', Router);
+
+app.get("/",(req,res)=>{
+res.send("hello")
+})
+
+
+const PORT = 8000;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+Connection(username, password);
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
